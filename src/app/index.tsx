9e416bbb6a7850c6195e388633157b87ad59adf1
ArturoZAC <arturoyz2105@gmail.com>
2026-07-23 15:56:00 -0500

    chore: upload initial project

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View className="mt-96 flex items-center justify-center">
+      <Text className="text-3xl font-black text-blue-500">Hello Friend...</Text>
+    </View>
+  );
+};
+
+export default App;
